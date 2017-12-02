5e51702b487a3e429fb6fe4005c2ffea2808599d
albracu <yo@alfredobravocuero.co>
2017-12-02 02:05:27 -0500

    Se crea archivo si no existe con informacion.

--- src/files/Archivos.java
+++ src/files/Archivos.java
@@ -1,10 +1,12 @@
 package files;
 
 import java.io.File;
+import java.util.*;
 
 public class Archivos {
     public static void main(String[] args) {
         File archivo = new File("/Users/alfredobravocuero/Desktop/pruebas/archivo.txt");
+        final Formatter nuevoarchivo;
 
         if(archivo.exists()){
             if (archivo.canWrite()){
@@ -13,7 +15,18 @@ public class Archivos {
                 System.out.println("El Archivo Existe pero no se puede Escribir");
             }
         }else {
-            System.out.println("El Archivo No Existe");
+
+            try{
+
+                nuevoarchivo = new Formatter("/Users/alfredobravocuero/Desktop/pruebas/archivo.txt");
+                System.out.println("El archivo se ha creado con Exito");
+                nuevoarchivo.format("%s %s %s", "Alfredo", "Bravo", "32");
+                nuevoarchivo.close();
+
+            }catch (Exception e){
+                System.out.println("Error: " + e);
+            }
+
         }
     }
 }
